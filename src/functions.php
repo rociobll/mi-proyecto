07c4d0f577fe5b30008df6370fc7ad42b77227a7
rociobll <roball@alumnatflorida.es>
2024-09-18 11:15:34 +0200

    Modifico función mensaje del archivo functions.php desde rama master

--- src/functions.php
+++ src/functions.php
@@ -5,7 +5,8 @@ function saludar() {
 }
 
 function mensaje() {
-    return "Este es un mensaje personalizado pto.9";
+    return "Este es un mensaje personalizado pto.9, y ahora lo modifico para crear conflicto 
+    con rama funcionalidad-1 pto.12";
 }
 
 ?>
