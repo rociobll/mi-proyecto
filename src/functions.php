9b4b5b09db73604353f16854654736209406bb53
rociobll <roball@alumnatflorida.es>
2024-09-18 11:27:04 +0200

    Modifico función mensaje de archivo functions.php desde rama funcionalidad-1

--- src/functions.php
+++ src/functions.php
@@ -5,7 +5,8 @@ function saludar() {
 }
 
 function mensaje() {
-    return "Este es un mensaje personalizado pto.9";
+    return "Este es un mensaje personalizado pto.9. Ahora lo modifico desde rama funcionalidad
+    para crear conflicto de fusión - pto.12";
 }
 
 ?>
